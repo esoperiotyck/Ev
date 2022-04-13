6e0de463502e55aec9b5eff5a97b7421fb49e87e
Evytsu <103592621+Evytsu@users.noreply.github.com>
2022-04-13 16:07:07 -0300

    Update test.py

--- test.py
+++ test.py
@@ -15,4 +15,4 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=68, axle_track=180)
 while True:
   threshold1 = line_sensor.reflection() - 25
   threshold2 = threshold1*-0.6
-  robot.drive(-100, -threshold2)
+  robot.drive(-100, -threshold2*2)
